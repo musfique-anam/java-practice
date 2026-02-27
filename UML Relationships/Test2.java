8a4ff6a33d51098ca348883e5145c4734ddbd08f
musfique-anam <m.musfique2002@gmail.com>
2026-02-27 12:29:26 +0600

    uml Test2 modified

--- UML Relationships/Test2.java
+++ UML Relationships/Test2.java
@@ -15,3 +15,24 @@ public class Test2 {
         t.student = s;
     }
 }
+
+
+// class Student {
+//     String name;
+// }
+
+// class Teacher {
+//     Student student;  // storing reference
+// }
+
+// public class Test {
+//     public static void main(String[] args) {
+//         Student s = new Student();  // object created
+//         s.name = "Rahim";
+
+//         Teacher t = new Teacher();
+//         t.student = s;  // storing reference inside Teacher
+
+//         System.out.println(t.student.name);
+//     }
+// }
